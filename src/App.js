2d2db98e41549edadbffac400dae58c4fbecac7e
Lerish-vZ <lerishavz@gmail.com>
2024-01-07 12:30:46 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Gallery from "./portfolio-gallery/Gallery";
 function App() {
-  render(){
+  //render(){
   return (
     <Gallery />
   );
